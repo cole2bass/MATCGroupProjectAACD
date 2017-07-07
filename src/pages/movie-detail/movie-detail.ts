57c48c9b5c0665ff7eaf1441e38c4942287db3ab
Adrien <admanaus@gmail.com>
2017-07-07 17:57:38 -0600

    User Detail Page and Movie Detail Page

--- src/pages/movie-detail/movie-detail.ts
+++ src/pages/movie-detail/movie-detail.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Http} from "@angular/http";
 
 /**
  * Generated class for the MovieDetailPage page.
@@ -14,11 +15,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MovieDetailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  sampleMovie: any;
+  imageBaseUrl: string = "https://image.tmdb.org/t/p/w200";
+  imageUrl: string;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private http: Http
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MovieDetailPage');
+    this.tempMovieService().subscribe(response => {
+      this.sampleMovie = JSON.parse(response.text());
+      this.imageUrl = this.imageBaseUrl + this.sampleMovie.poster_path;
+      console.log(this.sampleMovie);
+    })
+  }
+
+  tempMovieService(){
+    let url: string = "https://api.themoviedb.org/3/movie/76341?api_key=9d1d4c863da80cfbbfdfc5d7b3c456b0";
+    return this.http.get(url);
   }
 
 }
